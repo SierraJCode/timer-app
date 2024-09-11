c933c19e5e7009f42e554080eb73169a337705ab
Jesus Sierra <sierraja06@gmail.com>
2024-09-10 23:33:39 -0500

    Component Background was created

---
+++ src/Components/Background.tsx
@@ -0,0 +1,11 @@
+const Background = () => {
+  const bg = {
+    backgroundImage:
+      "url('https://static.vecteezy.com/system/resources/previews/028/663/748/non_2x/ai-ai-generatedrealistic-4k-sky-with-serene-cumulus-clouds-nature-s-atmospheric-beauty-in-stunning-detail-ideal-for-calming-and-scenic-concepts-free-photo.jpeg')",
+    backgroundSize: "cover",
+    height: "100vh",
+  };
+  return <div style={bg}></div>;
+};
+
+export default Background;
